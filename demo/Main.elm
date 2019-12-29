e516bc47207567ea9e30a798cd7fa67340bf1d84
James Carlson <jxxcarlson@mac.com>
2019-12-29 11:16:56 +0100

    Add Jabberwocky text for testing

--- demo/Main.elm
+++ demo/Main.elm
@@ -31,20 +31,37 @@ type alias Model =
     , lastKeyPress : Maybe String
     }
 
+text2 = """’Twas brillig, and the slithy toves
+      Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+      And the mome raths outgrabe.
+
+“Beware the Jabberwock, my son!
+      The jaws that bite, the claws that catch!
+Beware the Jubjub bird, and shun
+      The frumious Bandersnatch!”
+
+He took his vorpal sword in hand;
+      Long time the manxome foe he sought—
+So rested he by the Tumtum tree
+      And stood awhile in thought.
+"""
+
+text1 = """0 aaa
+        1 bbb
+        2 ccc
+        3 ddd
+        4 eee
+        5 fff
+        6 ggg
+        7 hhh
+        8 iii
+        9 jjj
+        """
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { content = Buffer.init """0 aaa
-1 bbb
-2 ccc
-3 ddd
-4 eee
-5 fff
-6 ggg
-7 hhh
-8 iii
-9 jjj
-"""
+    ( { content = Buffer.init text2
       , editor = Editor.init
       , lastKeyPress = Nothing
       }

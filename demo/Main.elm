f8eec703495223f6f1d41676afc187bd484ee4c3
James Carlson <jxxcarlson@mac.com>
2019-12-30 02:01:43 +0100

    UI tweaks

--- demo/Main.elm
+++ demo/Main.elm
@@ -103,7 +103,7 @@ view model =
         , div [HA.style "margin-top" "20px"] [
            Html.a [Attributes.href "https://github.com/jxxcarlson/elm-text-editor"] [text "Source code"]
            , text " — plenty of issues to work on.  This is a fork from code of SydneyNemzer"
-           , text "ctrl-c to copy selection; ctrl-p to paste copied text."
+           , div [HA.style "margin-top" "20px"] [text "ctrl-c to copy selection; ctrl-p to paste copied text."]
            ]
 
         , details [HA.style "margin-top" "20px"]
@@ -111,7 +111,7 @@ view model =
                 [ text "Debug" ]
 
             , div [HA.style "margin-top" "20px"] [text <| "window: " ++ Debug.toString (Editor.internal model.editor).window]
-            , div [] [text <| "cursor:  " ++ Debug.toString (Editor.internal model.editor).cursor]
+            , div [HA.style "margin-top" "5px"] [text <| "cursor:  " ++ Debug.toString (Editor.internal model.editor).cursor]
 
             , case model.lastKeyPress of
                 Just key ->

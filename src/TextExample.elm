16c30a26887aef305c939ab5eb9d6d48ce526669
James Carlson <jxxcarlson@mac.com>
2019-12-31 10:46:26 +0100

    New test text (alphabet)

--- src/TextExample.elm
+++ src/TextExample.elm
@@ -1,6 +1,43 @@
-module TextExample exposing (text1, text2)
+module TextExample exposing (text1, text2, text3)
 
+text3 = """1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
 
+"""
 
 text2 = """Jabberwocky
 

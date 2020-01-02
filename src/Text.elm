f50654ce13e3e8ddd991d3f96f0b70282da65675
James Carlson <jxxcarlson@mac.com>
2020-01-02 06:05:32 +0100

    Change text

--- src/Text.elm
+++ src/Text.elm
@@ -1,11 +1,16 @@
-module Text exposing (jabberwocky, alphabet, testString, news, tolstoy)
+module Text exposing (jabberwocky, alphabet, testString, gettysburgAddress, tolstoy)
 
-news = """The siege by supporters of an Iranian-backed militia at the U.S. Embassy in Baghdad ended Wednesday after the militia ordered them to withdraw, bringing relief to the diplomats trapped inside and averting a potential showdown between the United States and Iran.
+gettysburgAddress = """
+Below is Abraham Lincoln's Gettysburg Address.
+It was loaded as three long lines.  This example
+illustrates the current state of the text-wrap
+functionality.  It is based on Folkert de Vries'
+elm-paragraph library.
 
-Supporters of the Kataib Hezbollah militia who had spent the night camped outside the embassy dismantled their tents and marched out of the area, saying they would instead continue to press for the withdrawal of U.S. troops from Iraq in the nation’s parliament.
+Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.
+Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.
 
-“Yay! We burned them!” they chanted as they headed away from the embassy, a reference to the fires set by the demonstrators that burned two embassy reception areas Tuesday.
-"""
+But, in a larger sense, we can not dedicate—we can not consecrate—we can not hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom—and that government of the people, by the people, for the people, shall not perish from the earth."""
 
 
 tolstoy = """“Well, Prince, so Genoa and Lucca are now just family

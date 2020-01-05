25b6c619d490ba133724220a7b72ddf9ad43d4e4
James Carlson <jxxcarlson@mac.com>
2020-01-05 14:29:10 +0100

    default

--- demo/outside.js
+++ demo/outside.js
@@ -1,43 +1,26 @@
 // OUTSIDE
 
-navigator.permissions.query({
-  name: 'clipboard-read'
-}).then(permissionStatus => {
-  // Will be 'granted', 'denied' or 'prompt':
-  console.log(permissionStatus.state);
-  // setup app ports here ...
-  // Listen for changes to the permission state
+app.ports.infoForOutside.subscribe(msg => {
 
-        app.ports.infoForOutside.subscribe(msg => {
+    console.log("app.ports.infoForOutside")
 
-            console.log("app.ports.infoForOutside")
+    switch(msg.tag) {
 
-            switch(msg.tag) {
+        case "AskForClipBoard":
+        console.log("AskForClipBoard")
+        navigator.clipboard.readText()
+          .then(text => {
+            console.log('Clipboard (outside):', text);
+            app.ports.infoForElm.send({tag: "GotClipboard", data:  text})
+          })
+          .catch(err => {
+            console.error('Failed to read clipboard: ', err);
+          });
 
-                case "AskForClipBoard":
-                console.log("AskForClipBoard")
-                navigator.clipboard.readText()
-                  .then(text => {
-                    console.log('Clipboard (outside):', text);
-                    app.ports.infoForElm.send({tag: "GotClipboard", data:  text})
-                  })
-                  .catch(err => {
-                    console.error('Failed to read clipboard: ', err);
-                  });
+        break;
 
-                break;
+    }
 
-            }
-
-        })
-
-  permissionStatus.onchange = () => {
-    console.log(permissionStatus.state);
-    // remove app ports if permission is revoked ...
-  };
-});
-
-
-
+})
 
 
